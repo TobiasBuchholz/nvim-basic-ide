947467425335114fa12ebd802d5323ed3f9f2598
Tobias Buchholz <tobias.buchholz89@gmail.com>
2023-03-28 17:30:52 +0200

    Add markdown preview plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -60,6 +60,7 @@ return packer.startup(function(use)
   use { "lukas-reineke/indent-blankline.nvim", commit = "db7cbcb40cc00fc5d6074d7569fb37197705e7f6" }
   use { "goolord/alpha-nvim", commit = "0bb6fc0646bcd1cdb4639737a1cee8d6e08bcc31" }
   use { "ggandor/lightspeed.nvim" }
+  use({ "iamcco/markdown-preview.nvim", run = function() vim.fn["mkdp#util#install"]() end, })
 
   -- Colorschemes
   use { "lunarvim/darkplus.nvim", commit = "13ef9daad28d3cf6c5e793acfc16ddbf456e1c83" }

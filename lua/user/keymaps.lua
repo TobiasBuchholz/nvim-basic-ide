fbe9609ff197c113d1136c7a21e70571feb88f13
Tobias Buchholz <tobias.buchholz89@gmail.com>
2023-04-20 10:51:34 +0200

    Use BufferLineCycle and Move keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -29,8 +29,10 @@ keymap("n", "<M-S-l>", ":vertical resize -2<CR>", opts)
 keymap("n", "<M-S-h>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<S-l>", ":BufferLineCycleNext<CR>", opts)
+keymap("n", "<S-h>", ":BufferLineCyclePrev<CR>", opts)
+keymap("n", "<M-S-l>", ":BufferLineMoveNext<CR>", opts)
+keymap("n", "<M-S-h>", ":BufferLineMovePrev<CR>", opts)
 
 -- Clear highlights
 keymap("n", "<leader>h", "<cmd>nohlsearch<CR>", opts)
